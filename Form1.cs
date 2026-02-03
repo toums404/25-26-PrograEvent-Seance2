bcee51c8e2e3cad6ddd024312818c1cf554bc81e
Tom Thunus <tomthunus2@gmail.com>
2026-02-03 09:11:19 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace _25_26_PrograEvent_Seance2
+{
+    public partial class FenetrePrincipale : Form
+    {
+        public FenetrePrincipale()
+        {
+            InitializeComponent();
+        }
+    }
+}
